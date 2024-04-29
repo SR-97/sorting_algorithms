78955560e8d95f9af00bee018533a36de07f0a2d
SR-97 <sal.rh@hotmail.com>
2024-04-28 17:18:57 -0700

    s

--- sort.h
+++ sort.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/* Struct for doubly linked list */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
 /* Provided print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -18,13 +26,5 @@ void selection_sort(int *array, size_t size);
 
 /* Quick Sort function */
 void quick_sort(int *array, size_t size);
-
-/* Struct for doubly linked list */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
 
 #endif /* SORT_H */
